7451a52f4636aca6db069fbfbea9c25a8b7557a5
marcoatjunior <marcotaborda.jr@gmail.com>
2018-11-01 13:17:08 -0300

    adjusting namespace

--- src/DynamicFormWidget.php
+++ src/DynamicFormWidget.php
@@ -5,7 +5,7 @@
  * @license   https://github.com/wbraganca/yii2-dynamicform/blob/master/LICENSE
  */
 
-namespace wbraganca\dynamicform;
+namespace marcoatjunior\dynamicform;
 
 use Yii;
 use Symfony\Component\DomCrawler\Crawler;
